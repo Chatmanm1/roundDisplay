1ff8054e0e48b3ee479e96eec01d4137b4f4fc04
Matcha <96433082+Chatmanm1@users.noreply.github.com>
2023-09-26 08:48:25 -0500

    Getting weather, cpu , and ram all in TKinter

--- getInfo.py
+++ getInfo.py
@@ -2,30 +2,32 @@
 import tkinter as tk
 import time
 import psutil
-
-
-
+import requests
+from bs4 import BeautifulSoup
 
 
 
 class App():
     def __init__(self):
         self.root = tk.Tk()
-        self.label = tk.Label(text="Uptime")
-        self.label.pack()
+      
         self.label1 = tk.Label(text="CPU")
         self.label1.pack()
-        self.update_clock()
+       
         self.update_cpu()
         self.label2 = tk.Label(text="Memory")
         self.label2.pack()
         self.update_mem()
-        self.root.mainloop()
+        
+        self.label3 = tk.Label(text="")
+        self.label3.pack()
+        self.getWeather()
 
-    def update_clock(self):
-        now = time.strftime("%H:%M:%S")
-        self.label.configure(text=now)
-        self.root.after(4000, self.update_clock)
+        
+        self.root.mainloop()
+        
+
+
     def update_cpu(self):
         cpu = psutil.cpu_percent(interval=1)
         self.label1.configure(text=cpu)
@@ -34,9 +36,25 @@ class App():
         mem = psutil.virtual_memory().percent
         self.label2.configure(text=mem)
         self.root.after(4000, self.update_mem)
-        
+    def getWeather(self):
+        # enter city name
+        city = "Des Moines"
+        url = "https://www.google.com/search?q="+"weather"+city
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+        str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+        data = str.split('\n')
+        time = data[0]
+        sky = data[1]
+        self.label3.configure(text=temp+" "+sky)
+        self.root.after(30000, self.getWeather)
+          
     
+
 app=App()
+
+
 
 
 
