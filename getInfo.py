c159bcc421219a4b9089c9e29a3c77ce72ec0010
Matcha <96433082+Chatmanm1@users.noreply.github.com>
2023-10-01 10:00:03 -0500

    idk why some does and some doesn't work

--- getInfo.py
+++ getInfo.py
@@ -58,4 +58,54 @@ app=App()
 
 
 
+this works below here 
+# for python 3.x use 'tkinter' rather than 'Tkinter'
+import tkinter as tk
+import time
+import psutil
+import requests
+from bs4 import BeautifulSoup
+import time
+import serial
+
+ser = serial.Serial("COM11", 9600)
+
+
+
+
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.label1 = tk.Label(text="CPU")
+        self.label1.pack()
+        self.update_cpu()
+        self.label2 = tk.Label(text="Memory")
+        self.label2.pack()
+        self.update_mem()
+        self.label3 = tk.Label(text="")
+        self.label3.pack()
+        self.root.mainloop()
+    def update_cpu(self):
+        cpu = psutil.cpu_percent(interval=1)
+        self.label1.configure(text=str(cpu)+"% CPU")
+        self.root.after(10000, self.update_cpu)
+        cpuString = str(cpu).encode()
+        ser.write(cpuString)
+        ser.write(b'% CPU')
+    def update_mem(self):
+        mem = psutil.virtual_memory().percent
+        self.label2.configure(text=str(mem)+"% mem")
+        self.root.after(15000, self.update_mem)
+        memString = str(mem).encode()
+        ser.write(memString)
+        ser.write(b'% MEM')
+
+  
+              
     
+
+app=App()
+
+
+
+
