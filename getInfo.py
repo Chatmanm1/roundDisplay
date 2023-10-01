0fa51f706adf4b541dfa66769af184b0f93039dd
Matcha <96433082+Chatmanm1@users.noreply.github.com>
2023-10-01 13:16:51 -0500

    backup stuff

--- getInfo.py
+++ getInfo.py
@@ -58,7 +58,7 @@ app=App()
 
 
 
-this works below here 
+#this works below here 
 # for python 3.x use 'tkinter' rather than 'Tkinter'
 import tkinter as tk
 import time
@@ -109,3 +109,51 @@ app=App()
 
 
 
+
+
+this is the bit for arduino last aflashed with
+    
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_GC9A01A.h"
+ 
+#define TFT_DC 7
+#define TFT_CS 10
+#define TFT_DIN 15
+#define TFT_CLK 14
+
+// Hardware SPI on Feather or other boards
+Adafruit_GC9A01A tft(TFT_CS, TFT_DC,TFT_DIN,TFT_CLK);
+ String teststr = "";// for incoming serial data
+void setup() {
+  Serial.begin(9600);
+  Serial.println("GC9A01A Test!");
+ 
+  tft.begin();
+}
+ 
+void loop(void) {
+    tft.setRotation(2);
+  writeText();
+}
+ 
+ 
+
+
+ 
+
+unsigned long writeText() {
+  tft.setTextColor(GC9A01A_WHITE);
+  tft.setCursor(30, 120);
+  tft.setTextSize(2);
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+     tft.fillScreen(GC9A01A_BLACK);
+    // read the incoming byte:
+      teststr = Serial.readString();
+    // say what you got:
+   }
+   tft.println(teststr);
+ 
+ 
+}
